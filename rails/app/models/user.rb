35a602abda8cde16ce9a7c014da9644b4d99b88c
mametaro99 <bearsmichimato99@gmail.com>
2024-01-10 18:48:04 +0900

    記事の新規または更新登録のアクションAPIを作りました。

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
   has_many :articles, dependent: :destroy
 end
